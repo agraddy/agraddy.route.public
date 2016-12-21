ebec033653c558c65f13e5afad2a42729cc5b3ae
Anthony Graddy <anthony@locationq.com>
2016-12-21 06:18:10 +0000

    Working

--- test/index.js
+++ test/index.js
@@ -1,7 +1,54 @@
 var tap = require('agraddy.test.tap')(__filename);
+var response = require('agraddy.test.res');
 
-var mod = require('../');
+var pub = require('../');
 
-tap.assert.equal(mod.main('result'), 'result', 'Should be equal.');
+process.chdir('test');
+
+(function() {
+	var routes = {};
+	var res = response();
+	var res2 = response();
+
+	routes.get = {};
+
+	pub(routes.get).on('loaded', function() {
+		res.on('finish', function() {
+			tap.assert.equal(res._body, 'about\n', 'Should serve the about.txt file.');
+		});
+
+		routes.get['/about.txt']({}, res);
+
+
+		res2.on('finish', function() {
+			tap.assert.equal(res2._body, 'second\n', 'Should serve the sub/second.css file.');
+		});
+
+		routes.get['/sub/second.css']({}, res2);
+	});
+})();
+
+(function() {
+	var routes = {};
+	var res = response();
+
+	routes.get = {};
+
+	function header(req, res, lug, cb) {
+		res.setHeader('X-Test', 'test');
+		cb();
+	}
+
+	pub(routes.get, [header]).on('loaded', function() {
+		res.on('finish', function() {
+			tap.assert.deepEqual(res._headers[0], {"X-Test": "test"}, 'Should get header from luggage plugin.');
+			tap.assert.equal(res._body, 'second\n', 'Should get file contents.');
+		});
+
+		routes.get['/sub/second.css']({}, res);
+	});
+})();
+
+
 
 
